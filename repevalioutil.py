ea7658fc20bbad04c56979fd3db31d8ed09edaf0
Hector Martinez Alonso <hector.martinezalonso@thomsonreuters.com>
2018-06-15 18:01:43 -0400

    reader up
    two parallel LSTM process the same sentence
    
    git ignore file commit
    
    ignore file for now
    
    loss not working, it seems we managed to retrieve the hidden state properly
    
    two parallel LSTM process the same sentence
    
    git ignore file commit
    
    ignore file for now
    
    loss not working, it seems we managed to retrieve the hidden state properly

--- repevalioutil.py
+++ repevalioutil.py
@@ -1,20 +1,31 @@
 
 import json
 from nltk.tokenize import wordpunct_tokenize
-
+import torch
 
 
 
 class Env():
     w_2_idx = dict()
     w_2_idx["_UNK_"] = 0
+    w_2_idx["EOS"] = 1
+
+
+    idx_2_w = dict()
+    idx_2_w[0]="_UNK_"
+    idx_2_w[1]="EOS"
+
+
     label_2_idx = dict()
+    idx_2_label = dict()
+
 
     @staticmethod
     def w2idx(w, update=True):
             if w not in Env.w_2_idx:
                 if update:
                     Env.w_2_idx[w] = len(Env.w_2_idx.keys())
+                    Env.idx_2_w[Env.w_2_idx[w]] = w
                 else:
                     w = "_UNK_"
             return Env.w_2_idx[w]
@@ -24,6 +35,7 @@ class Env():
     def label2idx(l):
             if l not in Env.label_2_idx:
                 Env.label_2_idx[l] = len(Env.label_2_idx.keys())
+                Env.idx_2_label[Env.label_2_idx[l]] = l
             return Env.label_2_idx[l]
 
 
@@ -37,16 +49,16 @@ class NLIEntry():
         self.sentence1_idx = [Env.w2idx(w,update) for w in self.sentence1]
         self.sentence2_idx = [Env.w2idx(w,update) for w in self.sentence2]
 
-
-
+    def get_joint_sentences_tensor(self):
+        EOS = [Env.w2idx("EOS",update=False)]
+        return torch.tensor(self.sentence1_idx + EOS + self.sentence2_idx + EOS,dtype=torch.long)
 
 def read_NLIEntries(jsonfile,update_w2idx=True):
-    """Json keys are
+    """the common Json keys across both datasets are
     ['annotator_labels',
    'genre',
    'gold_label',
    'pairID',
-   'promptID',
    'sentence1',
    'sentence1_binary_parse',
    'sentence1_parse',
@@ -55,28 +67,43 @@ def read_NLIEntries(jsonfile,update_w2idx=True):
    'sentence2_parse']
     """
     entries = []
-    for line in open(jsonfile).readlines()[:50]:
+    for line in open(jsonfile).readlines():
         j = json.loads(line)
-        #If we use SNLI it contains no genre
+        if (j["gold_label"] not in ["contradiction","entailment","neutral"]):
+            continue
+        #If we use SNLI it contains no genre, so we infer it is a caption
         if "genre" not in j:
             j["genre"]="caption"
         entries.append(NLIEntry(j["genre"],j["gold_label"],j["pairID"],j["sentence1"],j["sentence2"],update=update_w2idx))
-        print(entries[-1].sentence1)
-        print(entries[-1].sentence1_idx)
-
     return entries
 
 
 def main():
-    infile = "/Users/hector/Downloads/multinli_1.0/multinli_1.0_train.jsonl"
+    infile = "data/nli/multinli_1.0/multinli_1.0_train.jsonl"
     train_entries = read_NLIEntries(infile)
-    infile = "/Users/hector/Downloads/multinli_1.0/multinli_1.0_dev_matched.jsonl"
+    print(infile)
+    print(len(train_entries))
+    print(train_entries[-1].sentence1)
+    print(train_entries[-1].sentence1_idx)
+    infile = "data/nli/multinli_1.0/multinli_1.0_dev_matched.jsonl"
     dev_entries = read_NLIEntries(infile,update_w2idx=False)
+    print(infile)
+    print(len(dev_entries))
+    print(dev_entries[-1].sentence1)
+    print(dev_entries[-1].sentence1_idx)
 
-    infile = "/Users/hector/Downloads/snli_1.0/snli_1.0_train.jsonl"
+    infile = "data/nli/snli_1.0/snli_1.0_train.jsonl"
     train_entries = read_NLIEntries(infile)
-    infile = "/Users/hector/Downloads/snli_1.0/snli_1.0_dev.jsonl"
+    print(infile)
+    print(len(train_entries))
+    print(train_entries[-1].sentence1)
+    print(train_entries[-1].sentence1_idx)
+    infile = "data/nli/snli_1.0/snli_1.0_dev.jsonl"
     dev_entries = read_NLIEntries(infile,update_w2idx=False)
+    print(infile)
+    print(len(dev_entries))
+    print(dev_entries[-1].sentence1)
+    print(dev_entries[-1].sentence1_idx)
 
 if __name__ == '__main__':
     main()
---
+++ sentence_to_label_network.py
@@ -0,0 +1,85 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import copy
+
+from repevalioutil import Env, NLIEntry, read_NLIEntries
+
+infile = "data/nli/multinli_1.0/multinli_1.0_dev_matched.jsonl"
+dev_entries = read_NLIEntries(infile, update_w2idx=True)
+
+class LSTMTagger(nn.Module):
+
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim,hidden_dim)
+        self.hidden2tag = nn.Linear(hidden_dim,tagset_size)
+        self.hidden = self.init_hidden()
+
+
+    def init_hidden(self):
+        return (torch.zeros(1, 1, self.hidden_dim),
+                torch.zeros(1, 1, self.hidden_dim))
+
+    def forward(self,twosentences):
+        embeddings = self.word_embeddings(twosentences)
+        x = embeddings.view(len(twosentences), 1, -1)
+        #print(twosentences.size(),x.size())
+        lstm_out, self.hidden = self.lstm(
+            x, self.hidden)
+
+        y  = self.hidden2tag(lstm_out[-1])
+
+    # print("about to call softmax")
+        tag_scores = F.log_softmax(y,dim=1)
+        return tag_scores
+
+vocab_size = len(Env.w_2_idx.keys())
+model = LSTMTagger(10,10,vocab_size,3)
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+# with torch.no_grad():
+#     insentences = dev_entries[0].get_joint_sentences_tensor()
+#     print(insentences)
+#     tag_scores = model(insentences)
+#     print("tag scores",tag_scores)
+
+for epoch in range(30):  # again, normally you would NOT do 300 epochs, it is toy data
+    print("Starting epoch #",epoch,"\n")
+    counter = 0
+    avg_loss = 0.0
+    for entry in dev_entries[0:500]:
+        model.hidden = model.init_hidden()
+        sentence_in = entry.get_joint_sentences_tensor()
+        tag_idx = entry.gold_label
+        counter+=1
+
+        gold_tag_space = torch.tensor([tag_idx],dtype=torch.long)
+        # Step 1. Remember that Pytorch accumulates gradients.
+        # We need to clear them out before each instance
+        model.zero_grad()
+
+        # Also, we need to clear out the hidden state of the LSTM,
+        # detaching it from its history on the last instance.
+        model.hidden = model.init_hidden()
+
+        # Step 2. Get our inputs ready for the network, that is, turn them into
+        # Tensors of word indices.
+
+        # Step 3. Run our forward pass.
+        tag_scores = model(sentence_in)
+
+        # Step 4. Compute the loss, gradients, and update the parameters by
+        #  calling optimizer.step()
+        loss = loss_function(tag_scores, gold_tag_space)
+        avg_loss += loss.item()
+        if counter % 500 == 0:
+            print(avg_loss/counter)
+        loss.backward(retain_graph=True)
+        optimizer.step()
